77d86fffb961a93e0f2d8d6c28695e8a58aab3c9
Abhay Ganti <abhayganti0909@gmail.com>
2023-04-20 12:26:25 +0800

    template complete v1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center pd-3 text-black">Hello, Tailwind</h1>
+      <p className='text-center'>This is a very simple minimalistic starter boilerplate for any React.js projects with Tailwind styling so you don't have to hurt yourself with manual CSS</p>
+    </div>
+  );
+}
+
+export default App;
